eccf4b8724a3f648cd79c563a88b219655eb6f62
1aby <1abyr@naver.com>
2019-11-14 12:03:59 +0900

    Upload JPA Shop Project V2 - Search Member API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -41,6 +43,47 @@ public class MemberApiController {
         Member findMember = memberService.findOne(id);
         return new UpdateMemberResponse(findMember.getId(),findMember.getName());
     }
+
+    //조회 테스트 (아이디 검색)
+    @PostMapping("/api/v2/members/{id}")
+    public SearchMemberResponse searchMemberResponse(@PathVariable("id") Long id){
+        Member findMember = memberService.findOne(id);
+        return new SearchMemberResponse(id,findMember.getName());
+    }
+
+    //조회 테스트 (전체)
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    //조회 테스트 (전체)
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+    //조회 전체 DTO
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
+    //조회 테스트 아이디 검색 Response
+    @Data
+    @AllArgsConstructor
+    static class SearchMemberResponse {
+        private Long id;
+        private String name;
+    }
+
 
     @Data
     static class UpdateMemberRequest {
